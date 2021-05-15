4ad7e53789b9008fc836999a5855341ca8ba7601
Thyago Mota <tmota@msudenver.edu>
2021-05-15 10:36:34 -0600

    updates

--- seamagnet/employee/models.py
+++ seamagnet/employee/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.db import models  
 
-# Create your models here.
+class Employee(models.Model):  
+
+    id   = models.IntegerField 
+    name = models.CharField(max_length=50)  
+     
+    class Meta:  
+        db_table = "employees"  
